4f9610f0f86d827d8a19a4178fd77a270b62be55
Kyle Robinson <ky.robinson72@gmail.com>
2023-10-31 23:54:14 -0600

    added code to identify if someone was logged in and remove the user name from local storage if they clicked log out

--- startup-js/startup.js
+++ startup-js/startup.js
@@ -1,9 +1,20 @@
 function checkUsername() {
-    if (localStorage.getItem('userName') != '') {
+    const name = localStorage.getItem('userName');
+    if (!name) {
         document.getElementById('nav-login').innerHTML = 'Log in';
     }
     else {
         document.getElementById('nav-login').innerHTML = 'Log out';
+    }
+}
+
+function logOut() {
+    const loggedInButtonText = document.getElementById('nav-login');
+    
+    if (loggedInButtonText === 'Log out') {
+        document.addEventListener('click', function () {
+            localStorage.removeItem('userName');
+        });
     }
 }
 
