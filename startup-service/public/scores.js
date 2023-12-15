7a810168e14ad53ca2a4d71027f211310462a911
Kyle Robinson <ky.robinson72@gmail.com>
2023-12-14 23:41:25 -0700

    Included call to web service for random photos when visiting the scores page

--- startup-service/public/scores.js
+++ startup-service/public/scores.js
@@ -47,4 +47,23 @@ async function loadScores() {
     }
   }
   
+  function displayPicture() {
+    const random = Math.floor(Math.random() * 1000);
+    fetch(`https://picsum.photos/v2/list?page=${random}&limit=1`)
+      .then((response) => response.json())
+      .then((data) => {
+        const containerEl = document.querySelector('#picture');
+  
+        const width = containerEl.offsetWidth;
+        const height = containerEl.offsetHeight;
+  
+        const imgUrl = `https://picsum.photos/id/${data[0].id}/${width}/${height}?grayscale`;
+        const imgEl = document.createElement('img');
+        imgEl.setAttribute('src', imgUrl);
+        containerEl.appendChild(imgEl);
+      });
+  }
+  
+  displayPicture();
+  
   loadScores();
